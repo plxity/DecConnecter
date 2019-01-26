16b6e93df6f4340ad3c0dd1fea18e51ac460fb21
plxity <purutaneja.com@gmail.com>
2019-01-27 01:23:00 +0530

    login function

--- devconnect/client/src/actions/authActions.js
+++ devconnect/client/src/actions/authActions.js
@@ -1,5 +1,6 @@
 import {GET_ERRORS} from './types';
 import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 export const registerUser =(UserData,history)=> dispatch =>{
     axios
         .post('/api/users/register',UserData)
@@ -10,4 +11,28 @@ export const registerUser =(UserData,history)=> dispatch =>{
                 payload:err.response.data
             }))
   
+}
+
+export const loginUser =(UserData)=> dispatch =>{
+    axios
+        .post('/api/users/login',UserData)
+        .then(res=>{
+            const {token} = res.data;
+            localStorage.setItem('jwtToken',token);
+            setAuthToken(token);
+            const decoded = jwt_decode(token);
+            dispatch(setCurrentUser(decoded));
+        })
+        .catch(err=>
+            dispatch({
+                type:GET_ERRORS,
+                payload:err.response.data
+            }))
+}
+
+export const setCurrentUser =(token)=>{
+    return{
+        type: SET_CURRENT_USER,
+        payload:decoded
+    }
 }
