184b64fec868319856a15b0a11f547bd9da4a3d0
plxity <purutaneja.com@gmail.com>
2019-05-11 20:54:53 +0530

    backend refactoring

--- README.md
+++
@@ -1,2 +0,0 @@
-# DecConnecter
-An application where different developers can meet and know each other. It is based on MERN stack.
--- server.js
+++ server.js
@@ -1,49 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const path = require('path');
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
-
+const connectDB = require('./Config/db');
 const app = express();
 
-// Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+connectDB();
 
-// // DB Config
-const db = require('./config/keys').mongoURI;
+app.use(express.json({extented: false}));
+app.get('/',(req,res)=>{res.send("Running")});
 
-// Connect to MongoDB
-mongoose.Promise= global.Promise;
-mongoose
-  .connect(db)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+app.use('/api/users', require('./Routes/api/users'));
+app.use('/api/profile', require('./Routes/api/profile'));
+app.use('/api/post', require('./Routes/api/post'));
+app.use('/api/auth', require('./Routes/api/auth'));
 
-// Passport middleware
-app.use(passport.initialize());
+const PORT = process.env.PORT || 5000;
 
-// Passport Config
-require('./config/passport')(passport);
-
-// Use Routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
-
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-var port = process.env.PORT || 8000;
-
-app.listen(port, function() {
-  console.log("App is running on port " + port);
-});
+app.listen(PORT,()=>{
+    console.log("Its running on the port");
+})
